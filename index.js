11ba80f4bdd18ea1dd69acf350631a259359aba2
Roger Denardi <rluizdenardi@gmail.com>
2022-06-15 23:09:20 -0300

    mudando pos dos lutadores

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const shop = new Sprite({
 
 const player = new Fighter({
   position: {
-    x: 0,
+    x: 200,
     y: 0
   },
   velocity: {
@@ -88,7 +88,7 @@ const player = new Fighter({
 
 const enemy = new Fighter({
   position: {
-    x: 400,
+    x: 700,
     y: 100
   },
   velocity: {
